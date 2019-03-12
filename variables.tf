79990209b3650dcbce42eab86c2c495352c9323b
Joseph Siefers <joseph.siefers@gmail.com>
2019-03-12 12:09:31 -0400

    Set default for aws_region.

--- variables.tf
+++ variables.tf
@@ -113,7 +113,7 @@ variable "github_token" {
 
 variable "aws_region" {
   type        = "string"
-  default     = ""
+  default     = "us-east-1"
   description = "(Optional) AWS Region, e.g. us-east-1. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
 }
 
